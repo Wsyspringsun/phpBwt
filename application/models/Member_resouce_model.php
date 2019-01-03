8cac4d6f380fa25bff3ba7e5b327c483537f49fd
小果子 <335828436@qq.com>
2019-01-03 21:39:54 +0800

    no commit message

---
+++ application/models/Member_resouce_model.php
@@ -0,0 +1,21 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Member_resouce_model extends MY_Model
+{
+    private $table = 'member_resouce';
+
+    public function __construct()
+    {
+        parent::__construct($this->table);
+    }
+	
+	
+	
+
+    
+}
+
+?>
