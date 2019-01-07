186172928949599427d57cc3157e671212e4a6bc
小果子 <335828436@qq.com>
2019-01-07 18:31:01 +0800

    no commit message

--- application/controllers/bwt/Member.php
+++ application/controllers/bwt/Member.php
@@ -619,30 +619,16 @@ class Member extends CI_Controller
         return true;
     }
 	
-	public function upload(){
-			if($_FILES["file"]["error"]){
-				echo $_FILES["file"]["error"];    
-			}else{
-					if(($_FILES["file"]["type"]=="image/png"||$_FILES["file"]["type"]=="image/jpeg")&&$_FILES["file"]["size"]<1024000){
-							 //$filename ="./img/".time().$_FILES["file"]["name"];
-							 //$filename =iconv("UTF-8","gb2312",$filename);
-							 $filename =date('YmdHis').rand(1000,9999).'.jpg';
-							if(file_exists($filename)){
-								show300('该文件已存在');
-							}else{  
-									$config['upload_path']='./upload/';
-									$config['allowed_types']='gif|jpg|png';
-									$config['file_name']=$filename;
-									$this->load->library('upload', $config);
-									$this->upload->do_upload('file');
-									$data['picPath']=PHOTOPATH.$filename;
-									//return $data['picPath'];
-									show200($data);
-							}        
-					}else{
-						show300('文件类型不对');
-					}
-				}
+		/**
+     * @title 图片上传接口
+     * @desc  (图片上传接口)
+     * @input {"name":"yzm","require":"true","type":"int","desc":"验证码"}
+     * @output {"name":"code","type":"int","desc":"200:成功,300各种提示信息"}
+     * @output {"name":"data.picPath","require":"true","type":"string","desc":"图片路径"}
+     */
+	
+	public function do_upload(){
+		$this->upload();
 	}
 	 
 }
